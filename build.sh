2a069906a9926dbbe099989140ae3f2112575a30
Graham Place <grahamgplace@gmail.com>
2023-01-02 23:05:56 -0500

    remove selenium install from build script

--- build.sh
+++ build.sh
@@ -2,18 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install Chrome.
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-
-# Update our system
-apt-get -y update
-
-# Install Chrome
-apt-get -y install google-chrome-stable
-
-echo "$GOOGLE_CREDENTIAL" >> "credentials.json"
-
 poetry lock --no-update
 poetry env use $PYTHON_VERSION
 poetry install
