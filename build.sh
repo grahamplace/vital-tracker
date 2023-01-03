c3c27b57b7b10c5bad297419321a2933aa7d3a1b
Graham Place <grahamgplace@gmail.com>
2023-01-02 22:37:28 -0500

    No sudo use in build script

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -o errexit
 
 # Install Chrome.
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 
 # Update our system
-sudo apt-get -y update
+apt-get -y update
 
 # Install Chrome
-sudo apt-get -y install google-chrome-stable
+apt-get -y install google-chrome-stable
 
 echo "$GOOGLE_CREDENTIAL" >> "credentials.json"
 
