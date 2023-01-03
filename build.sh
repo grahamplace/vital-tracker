bb05cca2149e5e7503963529d1512da9612d1786
Graham Place <grahamgplace@gmail.com>
2023-01-02 22:33:52 -0500

    Install Chrome in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 # exit on error
 set -o errexit
 
+# Install Chrome.
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+
+# Update our system
+sudo apt-get -y update
+
+# Install Chrome
+sudo apt-get -y install google-chrome-stable
+
 echo "$GOOGLE_CREDENTIAL" >> "credentials.json"
 
 poetry lock --no-update
